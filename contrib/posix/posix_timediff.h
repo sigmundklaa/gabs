5a43b2835e8c8509c6bdbfde1bae97d0ef52cade
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2026-01-03 17:55:50 +0100

    contrib: posix: Add comments

--- contrib/posix/posix_timediff.h
+++ contrib/posix/posix_timediff.h
@@ -9,7 +9,16 @@
 
 GABS_BEGIN_DECL
 
-GABS_API int posix_timediff_resolve(uint64_t diff, struct timespec* spec);
+/**
+ * @brief Resolve time difference, in microseconds, to an absolute timespec
+ *
+ * @param diff Time difference in microseconds
+ * @param spec Output timespec, in absolute time
+ * @return int
+ * @retval 0 Success
+ * @retval !=0 Error code
+ */
+GABS_API int posix_timediff_resolve(uint64_t diff, struct timespec *spec);
 
 GABS_END_DECL
 
