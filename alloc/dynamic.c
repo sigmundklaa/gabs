7f2e6a8f075fe4986a38d2fc017a6d3bf57d83f2
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2026-01-06 16:09:29 +0100

    alloc: Support dynamic

--- alloc/dynamic.c
+++ alloc/dynamic.c
@@ -2,9 +2,9 @@
 #include <gabs/alloc.h>
 #include <gabs/alloc/dynamic.h>
 
-void gabs_dyn_allocator_init(gabs_dyn_allocator *allocator,
-                             gabs_dyn_allocator_alloc alloc,
-                             gabs_dyn_allocator_dealloc dealloc)
+void gabs_dyn_allocator_init__(struct gabs_dyn_allocator *allocator,
+                               gabs_dyn_allocator_alloc alloc,
+                               gabs_dyn_allocator_dealloc dealloc)
 {
         allocator->alloc = alloc;
         allocator->dealloc = dealloc;
@@ -12,7 +12,7 @@ void gabs_dyn_allocator_init(gabs_dyn_allocator *allocator,
         gabs_handle_init(&allocator->handle);
 }
 
-int gabs_alloc(const gabs_allocator_h *allocator, size_t size, void** mem)
+int gabs_alloc(const gabs_allocator_h *allocator, size_t size, void **mem)
 {
         return gabs_dyn_allocator_get(allocator)->alloc(allocator, size, mem);
 }
--- include/gabs/alloc/dynamic.h
+++ include/gabs/alloc/dynamic.h
@@ -6,15 +6,33 @@
 
 GABS_BEGIN_DECL
 
+#if GABS_CONFIG_DYN_ALLOC
 typedef int (*gabs_dyn_allocator_alloc)(const gabs_allocator_h *, size_t,
                                         void **);
 typedef int (*gabs_dyn_allocator_dealloc)(const gabs_allocator_h *, void *);
 
-typedef struct gabs_dyn_allocator {
+struct gabs_dyn_allocator {
         gabs_dyn_allocator_alloc alloc;
         gabs_dyn_allocator_dealloc dealloc;
         gabs_allocator_h handle;
-} gabs_dyn_allocator;
+};
+
+void gabs_dyn_allocator_init__(struct gabs_dyn_allocator *allocator,
+                               gabs_dyn_allocator_alloc alloc,
+                               gabs_dyn_allocator_dealloc dealloc);
+
+#define gabs_dyn_allocator_init(allocator_, alloc_, dealloc_)                  \
+        gabs_dyn_allocator_init__(allocator_, alloc_, dealloc_)
+#else
+struct gabs_dyn_allocator {
+        gabs_allocator_h handle;
+};
+
+#define gabs_dyn_allocator_init(allocator_, alloc_, dealloc_)                  \
+        gabs_handle_init(&(allocator_)->handle)
+#endif
+
+typedef struct gabs_dyn_allocator gabs_dyn_allocator;
 
 static inline const gabs_allocator_h *
 gabs_dyn_allocator_handle(const struct gabs_dyn_allocator *allocator)
@@ -27,10 +45,6 @@ gabs_dyn_allocator_get(const gabs_allocator_h *handle)
 {
         return gabs_container_of(handle, struct gabs_dyn_allocator, handle);
 }
-
-void gabs_dyn_allocator_init(gabs_dyn_allocator *allocator,
-                             gabs_dyn_allocator_alloc alloc,
-                             gabs_dyn_allocator_dealloc dealloc);
 
 GABS_END_DECL
 
