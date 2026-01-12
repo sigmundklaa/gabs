5e969cfb4a1433a74527c7d695cf63cc742bada3
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2026-01-12 10:21:52 +0100

    random: posix: Seed on init

--- random/posix/random_posix.c
+++ random/posix/random_posix.c
@@ -1,10 +1,19 @@
 
 
 #include <stdlib.h>
+#include <time.h>
 
 #include <gabs/random.h>
+#include <gabs/init.h>
 
 uint32_t gabs_random(void)
 {
         return random();
 }
+
+static void seed(void)
+{
+        srandom(time(NULL));
+}
+
+GABS_INIT(seed);
