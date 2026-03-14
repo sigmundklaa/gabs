984f153ab4eb68da0ab9477febc0c3832822b73f
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2026-03-14 22:13:33 +0100

    contrib: posix: init: Let user define fn body

--- contrib/posix/posix_init.h
+++ contrib/posix/posix_init.h
@@ -6,55 +6,39 @@
 
 GABS_BEGIN_DECL
 
-#define POSIX_INIT_2(func_, prio_)                                             \
-        static void __attribute__((constructor(100 + prio_)))                  \
-        gabs_init_##__LINE__##__(void)                                         \
-        {                                                                      \
-                func_();                                                       \
-        }
-
-#define POSIX_INIT_1(func_) POSIX_INIT_2(func_, 99)
-
-#define POSIX_INIT__(count_, ...)                                              \
-        GABS_CONCAT_2(POSIX_INIT_, count_)(__VA_ARGS__)
-
 /**
  * @brief Regster function to be invoked at applciation start.
  *
  * This is before after entering main(). Functions registered with a priority
- * number lower is executed first. Priority can be 0-99. If not specified,
- * the default priority is 99.
+ * number lower is executed first. Priority can be 0-99.
  *
- * @param func_ Function to be invoked
- * @param priority_ (Optional) priority. Default: 99
+ * Example usage:
+ * ```
+ * POSIX_INIT(50) {
+ *         my_init_function();
+ * }
+ * ```
  */
-#define POSIX_INIT(...)                                                        \
-        POSIX_INIT__(GABS_COUNT_VA_ARGS(__VA_ARGS__), __VA_ARGS__)
-
-#define POSIX_DEINIT_2(func_, prio_)                                           \
-        static void __attribute__((destructor(100 + prio_)))                   \
-        gabs_deinit_##__LINE__##__(void)                                       \
-        {                                                                      \
-                func_();                                                       \
-        }
-
-#define POSIX_DEINIT_1(func_) POSIX_DEINIT_2(func_, 99)
-
-#define POSIX_DEINIT__(count_, ...)                                            \
-        GABS_CONCAT_2(POSIX_DEINIT_, count_)(__VA_ARGS__)
+#define POSIX_INIT(prio_)                                                      \
+        static void __attribute__((constructor(100 + prio_)))                  \
+        gabs_init_##__LINE__##__(void)
 
 /**
  * @brief Regster function to be invoked at applciation exit.
  *
  * This is called after main() exits. Functions registered with a priority
- * number higher is executed first. Priority can be 0-99. If not specified,
- * the default priority is 99.
+ * number higher is executed first. Priority can be 0-99.
  *
- * @param func_ Function to be invoked
- * @param priority_ (Optional) priority. Default: 99
+ * Example usage:
+ * ```
+ * POSIX_DEINIT(50) {
+ *         my_deinit_function();
+ * }
+ * ```
  */
-#define POSIX_DEINIT(...)                                                      \
-        POSIX_DEINIT__(GABS_COUNT_VA_ARGS(__VA_ARGS__), __VA_ARGS__)
+#define POSIX_DEINIT(prio_)                                                    \
+        static void __attribute__((destructor(100 + prio_)))                   \
+        gabs_deinit_##__LINE__##__(void)
 
 GABS_END_DECL
 
