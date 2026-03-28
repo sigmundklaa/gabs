9586c9f891fa8bc9f9d97406ac29364a0a873e21
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2026-03-28 18:48:40 +0100

    timer: Zephyr fixes

--- timer/zephyr/gabs_timer_def.h
+++ timer/zephyr/gabs_timer_def.h
@@ -6,6 +6,15 @@
 #error "gabs_timer_def.h should not be included directly"
 #endif /* GABS_TIMER_INCLUDE_DEF */
 
+#include <zephyr/sys/mutex.h>
+#include <zephyr/sys/dlist.h>
+
+struct gabs_timer_zephyr_ctx {
+        sys_dlist_t list;
+        struct k_mutex lock;
+};
+
 typedef void *gabs_timer;
+typedef struct gabs_timer_zephyr_ctx gabs_timer_ctx;
 
 #endif /* GABS_TIMER_ZEPHYR_DEF_H__ */
--- timer/zephyr/timer_zephyr.c
+++ timer/zephyr/timer_zephyr.c
@@ -1,5 +1,6 @@
 
 #include <zephyr/kernel.h>
+#include <zephyr/sys/dlist.h>
 #include <zephyr/sys/bitarray.h>
 
 #include <gabs/timer.h>
@@ -10,14 +11,18 @@
 
 struct timer_info {
         struct k_work_delayable dwork;
-        struct k_work_sync sync;
 
         gabs_timer_cb callback;
         void *user_data;
+
+        sys_dnode_t dnode;
+        struct gabs_timer_zephyr_ctx *ctx;
 };
 
 static struct timer_info pool[CONFIG_GABS_TIMER_MAX];
 SYS_BITARRAY_DEFINE_STATIC(pool_bitarray, CONFIG_GABS_TIMER_MAX);
+
+static __thread struct timer_info *firing = NULL;
 
 static size_t to_idx(struct timer_info *info)
 {
@@ -52,9 +57,13 @@ static void timer_expiry(struct k_work *work)
         dwork = k_work_delayable_from_work(work);
         info = CONTAINER_OF(dwork, struct timer_info, dwork);
 
-        if (info->callback) {
+        firing = info;
+
+        if (info->callback && (k_work_busy_get(work) & K_WORK_CANCELING) == 0) {
                 info->callback(info, info->user_data);
         }
+
+        firing = NULL;
 }
 
 bool gabs_timer_okay(gabs_timer timer_arg)
@@ -62,15 +71,19 @@ bool gabs_timer_okay(gabs_timer timer_arg)
         return true;
 }
 
-gabs_timer gabs_timer_install(gabs_timer_cb cb, void *user_data)
+gabs_timer gabs_timer_install(struct gabs_timer_zephyr_ctx *ctx,
+                              gabs_timer_cb cb, void *user_data)
 {
         struct timer_info *info;
 
         info = reserve();
         info->callback = cb;
         info->user_data = user_data;
+        info->ctx = ctx;
 
-        k_work_init_delayable(&info->dwork, timer_expiry);
+        (void)k_mutex_lock(&ctx->lock, K_FOREVER);
+        sys_dlist_append(&ctx->list, &info->dnode);
+        (void)k_mutex_unlock(&ctx->lock);
 
         return info;
 }
@@ -78,8 +91,24 @@ gabs_timer gabs_timer_install(gabs_timer_cb cb, void *user_data)
 int gabs_timer_uninstall(gabs_timer timer)
 {
         struct timer_info *info = timer;
+        struct k_work_sync sync;
 
-        (void)k_work_cancel_delayable_sync(&info->dwork, &info->sync);
+        (void)k_mutex_lock(&info->ctx->lock, K_FOREVER);
+        sys_dlist_remove(&info->dnode);
+        (void)k_mutex_unlock(&info->ctx->lock);
+
+        /* If this is invokation originates from a work handler, we can't use
+         * the sync canceller so we use the async one instead.
+         *
+         * This ensures that the work handler does not fire the user-specified
+         * callback after this lock is released, which is a requirement
+         * for preventing potential use-after-free. */
+        if (info == firing) {
+                (void)k_work_cancel_delayable(&info->dwork);
+        } else {
+                (void)k_work_cancel_delayable_sync(&info->dwork, &sync);
+        }
+
         release(info);
 
         return 0;
@@ -133,3 +162,47 @@ bool gabs_timer_active(gabs_timer timer)
 
         return k_work_delayable_busy_get(&info->dwork);
 }
+
+int gabs_timer_ctx_init(struct gabs_timer_zephyr_ctx *ctx)
+{
+        sys_dlist_init(&ctx->list);
+        k_mutex_init(&ctx->lock);
+
+        return 0;
+}
+
+int gabs_timer_ctx_deinit(struct gabs_timer_zephyr_ctx *ctx)
+{
+        struct timer_info *info;
+        struct timer_info *nxt;
+        struct k_work_sync sync;
+
+        SYS_DLIST_FOR_EACH_CONTAINER_SAFE(&ctx->list, info, nxt, dnode)
+        {
+                sys_dlist_remove(&info->dnode);
+
+                (void)k_work_cancel_delayable_sync(&info->dwork, &sync);
+                release(info);
+        }
+
+        return 0;
+}
+
+/* Pre-initialize timers. This prevents re-initing work item between
+ * release/reserve, which could cause a problem if the work item is still being
+ * canceled. */
+static int init_timers(void)
+{
+        size_t i;
+        struct timer_info *cur;
+
+        for (i = 0; i < ARRAY_SIZE(pool); i++) {
+                cur = &pool[i];
+
+                k_work_init_delayable(&cur->dwork, timer_expiry);
+        }
+
+        return 0;
+}
+
+SYS_INIT(init_timers, POST_KERNEL, 0);
